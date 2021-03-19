01e7fdd32ac318ad35049e849de2de8c5a70d8f0
Erica Thompson <younique98@gmail.com>
2021-03-19 09:30:57 -0700

    styled the Navigation bar and Login Page. I also added styling to the Horoscope css and entered more data for the horoscope readings

--- src/components/horoscope/SignHoroscope.js
+++ src/components/horoscope/SignHoroscope.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect} from "react"
 import { useParams } from "react-router-dom";
 import { HoroscopeContext } from "./HoroscopeProvider"
+import "./Horoscope.css"
 
 
 export const SignHoroscope = () => {
@@ -18,15 +19,43 @@ export const SignHoroscope = () => {
 
     return (
        
-        <section> 
-         {console.log(horoscopeToday)}
-         {console.log(horoscopeYesterday)}
-         {console.log(horoscopeTomorrow)}
-            <p>{horoscopeToday.description}</p>
-            <p>{horoscopeYesterday.description}</p>
-            <p>{horoscopeTomorrow.description}</p>
-               // button onclick to run function to check what day they clicked it for and create object and create the comment and post that to the attribute
+        <section className="horoscopeValues"> 
+         <section className="horoscopeToday">
+             <p>Today's Horoscope</p>
+            <p>Date Range: {horoscopeToday.date_range}</p>
+            <p>Current Date: {horoscopeToday.current_date}</p>
+            <p>Description: {horoscopeToday.description}</p>
+            <p>Compatibility: {horoscopeToday.compatibility}</p>
+            <p>Mood: {horoscopeToday.mood}</p>
+            <p>Color: {horoscopeToday.color}</p>
+            <p>Lucky Number: {horoscopeToday.lucky_number}</p>
+            <p>Lucky Time: {horoscopeToday.lucky_time}</p>
+            </section>
+            <section className="horoscopeYesterday">
+                <p>Yesterday's Horoscope</p>
+            <p>Date Range: {horoscopeYesterday.date_range}</p>
+            <p>Current Date: {horoscopeYesterday.current_date}</p>
+            <p>Description: {horoscopeYesterday.description}</p>
+            <p>Compatibility: {horoscopeYesterday.compatibility}</p>
+            <p>Mood: {horoscopeYesterday.mood}</p>
+            <p>Color: {horoscopeYesterday.color}</p>
+            <p>Lucky Number: {horoscopeYesterday.lucky_number}</p>
+            <p>Lucky Time: {horoscopeYesterday.lucky_time}</p>
+            </section>
+            <section className="horoscopeTomorrow">
+                <p>Tomorrow's Horoscope</p>
+            <p>Date Range: {horoscopeTomorrow.date_range}</p>  
+            <p>Current Date: {horoscopeTomorrow.description}</p>
+            <p>Description: {horoscopeTomorrow.current_date}</p>
+            <p>Compatibility: {horoscopeTomorrow.compatibility}</p>
+            <p>Mood: {horoscopeTomorrow.mood}</p>
+            <p>Color: {horoscopeTomorrow.color}</p>
+            <p>Lucky Number: {horoscopeTomorrow.lucky_number}</p>
+            <p>Lucky Time: {horoscopeTomorrow.lucky_time}</p>
+            </section> 
+               
         </section>
     )
 
 }
+// button onclick to run function to check what day they clicked it for and create object and create the comment and post that to the attribute
